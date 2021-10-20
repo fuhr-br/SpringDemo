4b2389d482d73512cbb19fcdcd73f68dc15e19e3
anderson fuhr <andersonfuhr@yahoo.com.br>
2021-10-20 20:45:56 -0300

    add Dev Tools

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,26 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/formulario")
+	@GetMapping("/")
 	public String index() {
-		return "<form action=\"/pagina-processa-dados-do-form\" method=\"post\">\r\n<center>"
-				+ "    <div>\r\n<br>"
-				+ "        <label for=\"nome\">Nome:</label>\r\n"
-				+ "        <input type=\"text\" id=\"nome\" name=\"usuario_nome\" />\r\n"
-				+ "    </div>\r\n<br>"
-				+ "    <div>\r\n"
-				+ "        <label for=\"email\">E-mail:</label>\r\n"
-				+ "        <input type=\"email\" id=\"email\" name=\"usuario_email\" />\r\n"
-				+ "    </div>\r\n<br>"
-				+ "    <div>\r\n"
-				+ "        <label for=\"msg\">Mensagem:</label>\r\n"
-				+ "       <higth> <textarea id=\"msg\" name=\"usuario_msg\"></textarea>\r\n</higth>"
-				+ "    </div>\r\n"
-				+ "\r\n"
-				+ "    <div class=\"button\">\r\n"
-				+ "        <button type=\"submit\">Enviar sua mensagem</button></center>\r\n"
-				+ "    </div>\r\n"
-				+ "</form>";
+		return "Greetings from Spring Boot!";
 	}
 
 }
